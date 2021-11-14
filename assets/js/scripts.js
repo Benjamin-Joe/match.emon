3156cc848f3524143a976f71f788ab9ec54b6c38
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-14 18:08:43 +0000

    Add variables for id/items from index.html to scrtips.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+/* Information for production
+id = game-grid for play area
+id = w-l for win and lose message
+id = timer for countdown timer 
+id = moves for move counter
+*/
+
+var countdown = 60;
+var activeCard = [];
+var gridObjects = '';
+
+var timer = document.getElementById('timer');
+var gameGrid =document.getElementById('game-grid');
+var winOrLose = document.getElementById('w-l');
+var counter = document.getElementById('moves');
+
+var cardList = [
+    
+]
