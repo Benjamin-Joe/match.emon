2a777d0429966d2760e3d9052bc7792f6ae072bf
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-14 18:20:26 +0000

    Add game play images to scripts.js file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,5 +15,104 @@ var winOrLose = document.getElementById('w-l');
 var counter = document.getElementById('moves');
 
 var cardList = [
-    
+    {
+        id: 1,
+        name: 'articuna',
+        image: './assets/images/articuna.png'
+    },
+    {
+        id: 2,
+        name: 'articuna',
+        image: './assets/images/articuna.png'
+    },
+    {
+        id: 3,
+        name: 'ash-pikachu',
+        image: './assets/images/ash-pikachu.png'
+    },
+    {
+        id: 4,
+        name: 'ash-pikachu',
+        image: './assets/images/ash-pikachu.png'
+    },
+    {
+        id: 5,
+        name: 'charizard',
+        image: './assets/images/charizard.png'
+    },
+    {
+        id: 6,
+        name: 'charizard',
+        image: './assets/images/charizard.png'
+    },
+    {
+        id: 7,
+        name: 'charmander',
+        image: './assets/images/charmander.png'
+    },
+    {
+        id: 8,
+        name: 'charmander',
+        image: './assets/images/charmander.png'
+    },
+    {
+        id: 9,
+        name: 'jolteon',
+        image: './assets/images/jolteon.png'
+    },
+    {
+        id: 10,
+        name: 'jolteon',
+        image: './assets/images/jolteon.png'
+    },
+    {
+        id: 11,
+        name: 'mewtwo',
+        image: './assets/images/mewtwo.png'
+    },
+    {
+        id: 12,
+        name: 'mewtwo',
+        image: './assets/images/mewtwo.png'
+    },
+    {
+        id: 13,
+        name: 'munchlax',
+        image: './assets/images/munchlax.png'
+    },
+    {
+        id: 14,
+        name: 'munchlax',
+        image: './assets/images/munchlax.png'
+    },
+    {
+        id: 15,
+        name: 'pikachu',
+        image: './assets/images/pikachu.png'
+    },
+    {
+        id: 16,
+        name: 'pikachu',
+        image: './assets/images/pikachu.png'
+    },
+    {
+        id: 17,
+        name: 'psyduck',
+        image: './assets/images/psyduck.png'
+    },
+    {
+        id: 18,
+        name: 'psyduck',
+        image: './assets/images/psyduck.png'
+    },
+    {
+        id: 19,
+        name: 'sobble',
+        image: './assets/images/sobble.png'
+    },
+    {
+        id: 20,
+        name: 'sobble',
+        image: './assets/images/sobble.png'
+    },
 ]
