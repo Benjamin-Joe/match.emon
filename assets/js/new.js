2464323e3c0c115aeb185529959847a8340b211f
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-15 10:24:24 +0000

    Add function for seeing if a card has already been flipped or not

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,5 +1,7 @@
 var gameImages = [];
 var gameArray = [];
+var isCardFlipped = -1;
+var cardFlipped = [];
 var playGame = false;
 var beginButton = document.getElementById('start')
 var gameBoard = document.getElementById('game-grid')
@@ -8,9 +10,43 @@ var gameBoard = document.getElementById('game-grid')
 function board(){
     for (var i = 0; i <= (gameArray.length -1); i++){
         gameBoard.innerHTML += '<div class="game-square">'
-        gameBoard.innerHTML += '<img id="playing-cards' +i+'" src="./assets/images/card-rear/card-rear-image.jpg" onclick="selectCard('+i+')" class="flipCard"></div>';
+        gameBoard.innerHTML += '<img id="playing-cards' +i+'" src="./assets/images/card-rear/card-rear-image.jpg" onclick="selectCard('+i+', this)" class="flipCard"></div>';
 
     }
+}
+
+
+
+// Selecting Cards
+
+function selectCard(playingCard, info){
+     // Check if card is already flipped
+     if(!insideArray(info.id, cardFlipped)){
+         console.log('Not already picked ')
+     }else{
+         console.log(' Is already picked ');
+     }
+     if(isCardFlipped >= 0){
+       // Flipping second card
+        if(playingCard != isCardFlipped){
+            info.src = "./assets/images/"+gameArray[playingCard];
+            cardFlipped.push(info.id);
+            var secondCard = playingCard;
+            // Check for match
+
+        }
+
+    }else{
+        // Flipping first card
+        isCardFlipped = playingCard;
+        info.src = "./assets/images/"+gameArray[playingCard];
+        cardFlipped.push(info.id);
+    }
+
+}
+
+function insideArray(value, array){
+    return array.indexOf(value) > -1;
 }
 
 
