29bc2cf0f44cc3379db75713c327dc2170dab222
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-15 12:39:24 +0000

    create game lock to prevent multiple cards being turned over when two cards are flipped and awaiting confirmation of a match

--- assets/js/new.js
+++ assets/js/new.js
@@ -2,9 +2,12 @@ var gameImages = [];
 var gameArray = [];
 var isCardFlipped = -1;
 var cardFlipped = [];
+var cardTimer = '';
+var gameLock = false;
 var playGame = false;
 var beginButton = document.getElementById('start')
 var gameBoard = document.getElementById('game-grid')
+
 // Building the game board
 
 function board(){
@@ -28,16 +31,21 @@ function selectCard(playingCard, info){
      }
      if(isCardFlipped >= 0){
        // Flipping second card
-        if(playingCard != isCardFlipped){
+        if(playingCard != isCardFlipped && !gameLock){
+
             info.src = "./assets/images/"+gameArray[playingCard];
             cardFlipped.push(info.id);
             var secondCard = playingCard;
+            gameLock = true;
             if(cardFlipped[cardFlipped.length-1] == cardFlipped[cardFlipped.length-2]){
                 //Does Match
                 console.log('It is a match');
+                gameLock = false;
+                isCardFlipped = -1;
             }else {
                 // Doesn't match
                 console.log('not a match');
+                cardTimer = setInterval(hideCard, 4000);
             }
             // Check for match
 
@@ -51,6 +59,20 @@ function selectCard(playingCard, info){
     }
 
 }
+
+
+// Function for cards turning back over if not a match
+function hideCard(){
+    for(var i=0;i<2;i++){
+        var hideId = cardFlipped.pop();
+        document.getElementById(hideId).src = "./assets/images/card-rear/card-rear-image.jpg";
+        console.log(hideId);
+    }
+    clearInterval(cardTimer);
+    gameLock = false;
+    isCardFlipped = -1;
+}
+
 
 function insideArray(value, array){
     return array.indexOf(value) > -1;
